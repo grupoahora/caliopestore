a8a2efd1420972ef5332e5a15d7e0e2d09156335
kevin patiÃno <aantiinoo@gmail.com>
2021-09-08 19:28:04 -0500

    video 7 ok

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -27,9 +27,6 @@
         <div class="col-8 grid-margin stretch-card">
             <div class="card">
                 <div class="card-body">
-                    {{-- <div class="d-flex justify-content-between">
-                        <h4 class="card-title">Registro de productos</h4>
-                    </div> --}}
                     <div class="form-group">
                         <label for="name">Nombre</label>
                         <input type="text" name="name" id="name" class="form-control" aria-describedby="helpId"
@@ -57,30 +54,10 @@
                             rows="3"></textarea>
                     </div>
                     <div class="form-group">
-                        <label for="short_description">Descripción</label>
-                        <textarea class="form-control" name="short_description" id="short_description"
+                        <label for="long_description">Descripción</label>
+                        <textarea class="form-control" name="long_description" id="long_description"
                             rows="10"></textarea>
                     </div>
-                    {{-- <div class="form-group">
-                        <label for="provider_id">Proveedor</label>
-                        <select class="form-control" name="provider_id" id="provider_id">
-                            @foreach ($providers as $provider)
-                            <option value="{{$provider->id}}">{{$provider->name}}</option>
-                    @endforeach
-                    </select>
-                    </div> --}}
-                    {{--  <div class="custom-file mb-4">
-                        <input type="file" class="custom-file-input" name="picture" id="picture" lang="es">
-                        <label class="custom-file-label" for="image">Seleccionar Archivo</label>
-                    </div>  --}}
-                    {{-- <div class="card-body">
-                        <h4 class="card-title d-flex">Imagen de producto
-                            <small class="ml-auto align-self-end">
-                                <a href="dropify.html" class="font-weight-light" target="_blank">Seleccionar Archivo</a>
-                            </small>
-                        </h4>
-                        <input type="file" name="picture" id="picture" class="dropify" />
-                    </div> --}}
                 </div>
             </div>
         </div>
@@ -114,25 +91,21 @@
                 </div>
             </div>
         </div>
-        
-        
     </div>
-    
     <div class="row">
         <div class="col-12 grid-margin">
             <div class="card">
                 <div class="card-body">
-                    <h4 class="card-title">Your so fresh input file — Default version</h4>
+                    <h4 class="card-title">Imagenes de producto</h4>
                     <input type="file" name="images[]" class="dropify" multiple />
                 </div>
             </div>
         </div>
     </div>
     <button type="submit" class="btn btn-primary mr-2">Registrar</button>
-                     <a href="{{route('products.index')}}" class="btn 
-                     btn-light">
-                        Cancelar
-                     </a>
+    <a href="{{route('products.index')}}" class="btn btn-light">
+        Cancelar
+    </a>
     {!! Form::close() !!}
 </div>
 @endsection
@@ -144,6 +117,7 @@
 {!! Html::script('ckeditor/ckeditor.js') !!}
 <script>
     CKEDITOR.replace('long_description');
+
 </script>
 <script>
     $(document).ready(function () {
