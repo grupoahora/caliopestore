5ce50d68bc823860e64619e5504d8c99ec0b5dc1
kevin patiÃno <aantiinoo@gmail.com>
2021-09-15 16:54:03 -0500

    video18 ok2

--- resources/lang/es/auth.php
+++ resources/lang/es/auth.php
@@ -14,6 +14,6 @@ return [
     */
 
     'failed' => 'Estas credenciales no coinciden con nuestros registros.',
-    'throttle' => 'Demasiados intentos de acceso. Por favor inténtelo de nuevo en :seconds segundos.',
+    'throttle' => 'Demasiados intentos de acceso. Por favor inténtelo de nuevo en :seconds segsundos.',
 
 ];
