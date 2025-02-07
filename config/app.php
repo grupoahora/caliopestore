7e2cef3e6f13dbfed898b3531cfbe6d71e160b5b
grupoahora <aantiinoo@gmail.com>
2025-02-07 14:17:45 -0500

    dom pdf app.php

--- config/app.php
+++ config/app.php
@@ -244,7 +244,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
 
 
-        'PDF' => Barryvdh\DomPDF\Facade::class,
+        'PDF' => Barryvdh\DomPDF\Facade\Pdf::class,
 
         'DNS1D' => Milon\Barcode\Facades\DNS1DFacade::class,
         'DNS2D' => Milon\Barcode\Facades\DNS2DFacade::class,
